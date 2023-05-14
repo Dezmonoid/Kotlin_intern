77ec0ff85148ee3eed8d53cc3c51003b0d16143d
Dezmonoid <Dezmoonoid@mail.ru>
2023-05-14 13:39:14 +0300

    Доработка занания 3

--- src/main/kotlin/Task3.kt
+++ src/main/kotlin/Task3.kt
@@ -1,4 +1,5 @@
 import kotlin.math.*
+
 fun main() {
     print("Введите значение a = ")
     val a = readln().toDouble()
@@ -8,27 +9,33 @@ fun main() {
     val c = readln().toDouble()
     quadraticRoot(a, b, c)
 }
+
 fun sqr(n: Double): Double = n * n
 fun discriminant(a: Double, b: Double, c: Double): Double = sqr(b) - (4 * a * c)
-fun rootsNumber(a: Double, b: Double, c: Double) {
+fun rootsNumber(a: Double, b: Double, c: Double): Int {
     when {
-        discriminant(a, b, c) < 0.0 -> println("Корней нет.")
-        discriminant(a, b, c) == 0.0 -> println("Один корень:")
-        discriminant(a, b, c) > 0.0 -> println("Два корня:")
+        discriminant(a, b, c) < 0.0 -> return 0
+        discriminant(a, b, c) == 0.0 -> return 1
+        discriminant(a, b, c) > 0.0 -> return 2
     }
+    return 0
 }
 
 fun quadraticRoot(a: Double, b: Double, c: Double) {
-    val x1 : Double
-    val x2 : Double
-    rootsNumber(a,b,c)
-    if (discriminant(a, b, c) == 0.0) {
-        x1 = (-b + sqrt(discriminant(a, b, c))) / (2 * a)
-        println("Корень x1 = $x1")
-    } else {
-        x1 = (-b + sqrt(discriminant(a, b, c))) / (2 * a)
-        x2 = (-b - sqrt(discriminant(a, b, c))) / (2 * a)
-        println("Корень x1 = $x1")
-        println("Корень x2 = $x2")
+    val x1: Double
+    val x2: Double
+    when (rootsNumber(a, b, c)) {
+        0 -> println("Корней нет")
+        1 -> {
+            x1 = (-b + sqrt(discriminant(a, b, c))) / (2 * a)
+            println("Корень x1 = $x1")
+        }
+
+        2 -> {
+            x1 = (-b + sqrt(discriminant(a, b, c))) / (2 * a)
+            x2 = (-b - sqrt(discriminant(a, b, c))) / (2 * a)
+            println("Корень x1 = $x1")
+            println("Корень x2 = $x2")
+        }
     }
 }
