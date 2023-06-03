c82171dfc7445bb8c48af85ede8a2abca4efbc57
Dezmonoid <Dezmoonoid@mail.ru>
2023-06-03 21:13:00 +0300

    Внёс изменения в задание 11.2

--- src/main/kotlin/Task11_2.kt
+++ src/main/kotlin/Task11_2.kt
@@ -1,8 +1,8 @@
 fun main() {
     val concat = { stringOne: String, stringTwo: String, stringThree: String ->
-    val listOfString = listOf(stringOne,stringTwo,stringThree).sortedWith(comparator = { str1:String, str2:String -> str1.length-str2.length})
+    val listOfString = listOf(stringOne,stringTwo,stringThree).sortedBy { it.length }
         println(listOfString.joinToString())
     }
     println("Введите 3 слова:")
-    concat(readln().toString(),readln().toString(),readln().toString())
+    concat(readln(),readln(),readln())
 }
