ab421e1cea5bd429785f3106e1547586cdbbecd9
Dezmonoid <Dezmoonoid@mail.ru>
2023-06-05 21:00:16 +0300

    Выполнение задания 9_6

---
+++ src/main/kotlin/Task9_6.kt
@@ -0,0 +1,68 @@
+fun main() {
+    val singleDigits = listOf(null, "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять")
+    val doubleDigits = listOf(
+        null,
+        null,
+        "двадцать",
+        "тридцать",
+        "сорок",
+        "пятьдесят",
+        "шестьдесят",
+        "семьдесят",
+        "восемьдесят",
+        "девяноста"
+    )
+    val secondTen = listOf(
+        "десять",
+        "одинадцать",
+        "двенадцать",
+        "тринадцать",
+        "четырнадцать",
+        "пятнадцать",
+        "шестнадцать",
+        "семнадцать",
+        "восемьнадцать",
+        "девятнадцать"
+    )
+    println("Введите число от 1 до 100")
+    var userNumber: Int
+    while (true) {
+        userNumber = readln().toInt()
+        if (userNumber in 1..100) {
+            break
+        } else {
+            println("Вы ввели число не входящее в диапазон")
+        }
+    }
+    registrationTransfer(userNumber, singleDigits, secondTen, doubleDigits)
+
+}
+
+fun registrationTransfer(
+    userNumber: Int,
+    singleDigits: List<String?>,
+    secondTen: List<String?>,
+    doubleDigits: List<String?>
+) {
+    when (userNumber) {
+        in 1..9 -> {
+            println(singleDigits[userNumber])
+        }
+
+        in 10..19 -> {
+            println(secondTen[userNumber % 10])
+        }
+
+        in 20..99 -> {
+            if (userNumber % 10 == 0 && userNumber.div(10) == 2) {
+                println(doubleDigits[2])
+            } else {
+                println("${doubleDigits[userNumber.div(10)]} ${singleDigits[userNumber % 10]}")
+            }
+        }
+
+        else -> {
+            println("сто")
+        }
+    }
+}
