5062d83eddee0b3f0aa6e6fe73a110ddb8e0c35e
Dezmonoid <Dezmoonoid@mail.ru>
2023-06-05 21:20:58 +0300

    Внесение изменений в задании 8.1

--- src/main/kotlin/Task8_1.kt
+++ src/main/kotlin/Task8_1.kt
@@ -2,30 +2,18 @@ fun main() {
     println("Введите два числа и знак операции(*,/,+,-)")
     val numberOne: Int = readln().toInt()
     val numberTwo: Int = readln().toInt()
-    val operationType: String = readln()
-    when (operationType) {
-        "*" -> {
-            println("Результат вычисления: $numberOne * $numberTwo = ${numberOne * numberTwo}")
-        }
-
+    when (readln()) {
+        "*" -> println("Результат вычисления: $numberOne * $numberTwo = ${numberOne * numberTwo}")
         "/" -> {
             if (numberTwo != 0) {
-                println("Результат вычисления: $numberOne * $numberTwo = ${numberOne / numberTwo}")
+                println("Результат вычисления: $numberOne * $numberTwo = ${numberOne.toDouble() / numberTwo.toDouble()}")
             } else {
                 println("На ноль делить нельзя")
             }
         }
 
-        "+" -> {
-            println("Результат вычисления: $numberOne * $numberTwo = ${numberOne + numberTwo}")
-        }
-
-        "-" -> {
-            println("Результат вычисления: $numberOne * $numberTwo = ${numberOne - numberTwo}")
-        }
-
-        else -> {
-            println("Вы ввели неизвестную операцию")
-        }
+        "+" -> println("Результат вычисления: $numberOne * $numberTwo = ${numberOne + numberTwo}")
+        "-" -> println("Результат вычисления: $numberOne * $numberTwo = ${numberOne - numberTwo}")
+        else -> println("Вы ввели неизвестную операцию")
     }
 }
