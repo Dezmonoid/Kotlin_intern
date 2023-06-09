978a9fbd1300ae93f3c92bc6a4aae36d30d3ee57
Dezmonoid <Dezmoonoid@mail.ru>
2023-06-09 22:05:46 +0300

    Внесение изменений в задание 9.7

--- src/main/kotlin/Task9_7.kt
+++ src/main/kotlin/Task9_7.kt
@@ -1,7 +1,4 @@
 fun main() {
-    val time = "01:20:12"
-    val hour = time.split(':')[0].toInt()
-    val minute = time.split(':')[1].toInt()
-    val second = time.split(':')[2].toInt()
-    println("С начала дня прошло ${hour * 60 * 60 + minute * 60 + second} секунд")
+    val time = "01:20:12".split(':')
+    println("С начала дня прошло ${time[0].toInt() * 60 * 60 + time[1].toInt() * 60 + time[2].toInt()} секунд")
 }
