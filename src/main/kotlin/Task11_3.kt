512c6a7a61bc2831e08f3b0322ecc575e4d35d30
Dezmonoid <Dezmoonoid@mail.ru>
2023-06-11 20:56:16 +0300

    Внесение изменений в задание 11.3

--- src/main/kotlin/Task11_3.kt
+++ src/main/kotlin/Task11_3.kt
@@ -1,8 +1,9 @@
 fun main() {
-    replacement(readln()) { userText: String -> userText.replace(",", " блин") }
+    replacement(readln()) { userText: String -> userText.replace(oldValue = ",", newValue = " блин") }
 }
 
 fun replacement(message: String, convert: (String) -> String): String {
-    println(convert(message))
-    return convert(message)
+    val result = convert(message)
+    println(result)
+    return result
 }
